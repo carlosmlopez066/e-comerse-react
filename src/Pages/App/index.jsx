93bf3c3651b5b2a6465ddd5e90f8f27d4679d828
Carlos Lopez <carlosmlopez066@gmail.com>
2023-10-12 19:47:08 -0400

    enrutamiento rear router

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,26 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
-
 import '../../../App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-Account', element: <MyAccount /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
+  ])
+  return routes
+}
+
+const App = () => {
 
   return (
-    <>
-      <div className='bg-red-600'>
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-
-      </div>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
