45680629abbd2e17e35ed6846e9288fef42f2792
Carlos Lopez <carlosmlopez066@gmail.com>
2023-10-25 11:19:52 -0400

    OrderCart component

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react"
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context"
+import OrderCard from "../../Components/OrderCard"
 import "./styles.css"
 
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
+  console.log('cart', context.cartProducts)
   return (
     <aside
       className={`${context.ischeckoutSideMenuOpen ? "flex" : "hidden"} checkout-side-menu   items-center flex-col fixed right-0 border border-black rounded-lg bg-white`}
@@ -16,6 +18,17 @@ const CheckoutSideMenu = () => {
           <XMarkIcon onClick={() => context.closecheckoutSideMenu()} className="h-6 w-6 text-black cursor-pointer" />
         </div>
       </div>
+      {
+        context.cartProducts.map(product => (
+          <OrderCard
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+          />
+
+        ))
+      }
     </aside>
   )
 }
---
+++ src/Components/OrderCard/index.jsx
@@ -0,0 +1,20 @@
+import { XMarkIcon } from "@heroicons/react/24/solid"
+
+const OrderCard = props => {
+  const { title, image, price } = props
+  return (
+    <div className=" w-full p-3 flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img className="w-full h-full rounded-ls object-cover" src={image} alt={title} />
+        </figure>
+        <p className="text-sm font-light"></p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">{price}</p>
+        <XMarkIcon className="h-6 w-6 text-black cursor-pointer" />
+      </div>
+    </div>
+  )
+}
+export default OrderCard
